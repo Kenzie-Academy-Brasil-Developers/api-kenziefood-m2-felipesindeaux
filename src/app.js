26599a96262c74aefddff76a2510a1f465c30b84
Felipe Souza Sindeaux <89540255+felipesindeaux@users.noreply.github.com>
2022-01-25 17:17:10 -0300

    feat: dom-cards-pelo-db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 import { FoodController } from "./controllers/foodControllers.js"
-import { Cards } from "./components/cards.js"
-import { FoodRoutes } from "./routes/foods.js"
 
-FoodRoutes.get().then((data) => Cards.criarCards(data))
-
-// FoodController.saveProducts()
-// FoodController.montarCards()
+await FoodController.saveProducts()
+await FoodController.montarCards()
